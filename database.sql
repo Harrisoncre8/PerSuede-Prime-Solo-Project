670f7e1324129cffff3b8a0e525b41400ceb01bd
Harrison Nguyen <nguy2173@umn.edu>
2020-01-13 13:35:08 -0600

    Create database

--- database.sql
+++ database.sql
@@ -1,10 +1,37 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+-- Table that handles account data
+CREATE TABLE "account" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "zip-code" INT NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "clearance_level" INT NOT NULL DEFAULT 0
 );
+
+-- Table that handles weather data
+CREATE TABLE "weather" (
+    "id" SERIAL PRIMARY KEY,
+    "weather_type" VARCHAR,
+    "high" VARCHAR,
+    "low" VARCHAR,
+    "user_id" INT REFERENCES "account"
+);
+
+-- Table that holds all outfit data
+CREATE TABLE "outfits" (
+    "id" SERIAL PRIMARY KEY,
+    "url" VARCHAR NOT NULL,
+    "age" VARCHAR,
+    "weather_id" INT REFERENCES "weather"
+);
+
+-- Table that handles favorited data
+CREATE TABLE "favorite" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "account",
+    "outfits_id" INT REFERENCES "outfits"
+);
+
+-- SELECT "favorite"."id", "favorite"."user_id", "favorite"."outfits_id", "outfits"."url" FROM "favorite"
+-- JOIN "outfits" ON "outfits"."id" = "favorite"."outfits_id"
